c0d652b229849ea5d1442ef982f4be009294eb20
amatory1346 <154450659+amatory1346@users.noreply.github.com>
2025-11-15 21:38:26 +0100

    Update Solidity version from 0.8.24 to 0.8.30

--- Kindora.sol
+++ Kindora.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.30;
 
 /// @title Kindora ERC20 token with fees, liquidity and charity routing
 /// @notice ERC20 token extended with buy/sell fees, auto-liquidity and charity routing
